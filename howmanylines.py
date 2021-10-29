05308299f112b8b7d7b5d034437d457d73c1e378
Garrett Webb <Garrett@turboresources.com>
2021-10-29 08:12:08 -0700

    now it does subdirectories

--- howmanylines.py
+++ howmanylines.py
@@ -5,7 +5,7 @@ from tkinter import Tk
 from tkinter.filedialog import askdirectory
 
 numlines = 0
-file_extensions = [".c", ".cpp", ".h"]
+file_extensions = [".c", ".cpp", ".h", ".rc"]
 
 def rawcount(f):
     lines = 0
@@ -13,7 +13,7 @@ def rawcount(f):
     read_f = f.raw.read
     buf = read_f(buf_size)
     while buf:
-        lines += buf.count(b'\n')
+        lines += buf.count(b'\r\n')
         buf = read_f(buf_size)
     return lines
 
@@ -23,16 +23,16 @@ print(path)
 
 onlyfiles = [f for f in listdir(path) if isfile(join(path, f))]
 #print(onlyfiles)
-#for root, dirs, files in os.walk(path):
-for file in onlyfiles:
-    split_tup = os.path.splitext(file)
-    if (split_tup[1] in file_extensions):
-        #with open(os.path.join(root, file), "rb") as auto:
-        with open(os.path.join(path, file), "rb") as auto:
+for root, dirs, files in os.walk(path):
+    for file in files:
+        split_tup = os.path.splitext(file)
+        if (split_tup[1] in file_extensions):
+            with open(os.path.join(root, file), "rb") as auto:
+            #with open(os.path.join(path, file), "rb") as auto:
             #print(file)
-            lines = rawcount(auto)
-            numlines += lines
-            print("lines in",file, ":", lines)
+                lines = rawcount(auto)
+                numlines += lines
+                print("lines in",file, ":", lines)
 
 print("Total lines :", numlines)
 
